89159bd06b6de12385805b63c3876e6372364ff6
RebbD <kauavictor259@gmail.com>
2021-06-05 14:47:32 -0300

    slide automatico

--- js/body.js
+++ js/body.js
@@ -1,4 +1,4 @@
-let show = true
+let count = 1
 
 let list = document.getElementsByClassName('list')
 let radios = document.getElementsByClassName('radio')
@@ -9,19 +9,21 @@ let proj = document.getElementsByClassName('proj')
 
 list[0].addEventListener('click', () => {
     designBtn()
-    
+
+    count = 1
     radios[0].checked = true
 })
 
 list[1].addEventListener('click', () => {
     webBtn()
-    
+
+    count = 3
     radios[2].checked = true
 })
 
 list[2].addEventListener('click', () => {
     escolBtn()
-    
+    count = 5
     radios[4].checked = true
 })
 
@@ -31,6 +33,7 @@ function designBtn(){
     list[2].classList.remove('on')
 
     sele.style.marginRight = '136px'
+    
 }
 function webBtn(){
     list[0].classList.remove('on')
@@ -57,5 +60,24 @@ for(let i = 0;i < radios.length;i++){
         }else if(radios[4].checked || radios[5].checked){
             escolBtn()
         }
+        count = i + 1
     })
 }
+
+/* Automatic */
+
+setInterval(() => {
+    document.getElementById('radio' + count).checked = true
+    count++
+
+    if(count <= 3){
+        designBtn()
+    }else if(count <= 5){
+        webBtn()
+    }else if(count < 7){
+        escolBtn()
+    }else{
+        count = 1
+    }
+    
+}, 3000)
