6e3a1f20940f5f13d88282f74860e14f5ed26cd8
RebbD <kauavictor259@gmail.com>
2021-06-04 22:12:49 -0300

    slide terminado

--- js/body.js
+++ js/body.js
@@ -2,27 +2,10 @@ let show = true
 
 let web = document.getElementsByClassName('web')
 let design = document.getElementsByClassName('design')
+let radios = document.getElementsByClassName('radio')
 
 let proj = document.getElementsByClassName('proj')
 
-
-web[0].addEventListener('click', () => {
-    show = true
-    web[0].classList.toggle('on', show)
-
-    show = !show
-    design[0].classList.toggle('on', show)
-
-    show = false
-
-    if(!proj[1].classList.contains("on")){
-        proj[0].classList.toggle('on', show)
-
-        show = !show
-        proj[1].classList.toggle('on', show)
-    }
-
-})
 
 design[0].addEventListener('click', () => {
     show = true
@@ -30,14 +13,38 @@ design[0].addEventListener('click', () => {
 
     show = !show
     web[0].classList.toggle('on', show)
-
-    show = false
-
-    if(!proj[0].classList.contains("on")){
-        proj[1].classList.toggle('on', show)
-
-        show = !show
-        proj[0].classList.toggle('on', show)
-    }
-
+    
+    radios[0].checked = true
 })
+
+web[0].addEventListener('click', () => {
+    show = true
+    web[0].classList.toggle('on', show)
+
+    show = !show
+    design[0].classList.toggle('on', show)
+    
+    radios[2].checked = true
+})
+
+function designBtn(){
+    design[0].classList.add('on')
+
+    web[0].classList.remove('on')
+}
+function webBtn(){
+    design[0].classList.remove('on')
+
+    web[0].classList.add('on')
+}
+
+
+for(let i = 0;i < radios.length;i++){
+    radios[i].addEventListener('click',() => {
+        if(radios[0].checked || radios[1].checked){
+            designBtn()
+        }else if(radios[2].checked || radios[3].checked){
+            webBtn()
+        }
+    })
+}
